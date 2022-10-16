9c7073d85d8ae2a4b0703faa49573e5f012213d9
404Vector <tiryul@gmail.com>
2022-10-16 09:39:36 +0000

    add: add common modules

---
+++ Modules/Utility.py
@@ -0,0 +1,44 @@
+from tqdm import tqdm
+import random
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn import preprocessing
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+def competition_metric(true, pred):
+    return f1_score(true, pred, average="macro")
+
+def get_data(df, infer=False):
+    if infer:
+        return df['img_path'].values
+    return df['img_path'].values, df['artist'].values
+
+def inference(model, test_loader, device):
+    model.to(device)
+    model.eval()
+    
+    model_preds = []
+    
+    with torch.no_grad():
+        for item in tqdm(iter(test_loader)):
+            img = item['image'].float().to(device)
+            h = item['height']
+            w = item['width']
+            
+            model_pred = model(img)
+            model_preds += model_pred.argmax(1).detach().cpu().numpy().tolist()
+    
+    print('Done.')
+    return model_preds
