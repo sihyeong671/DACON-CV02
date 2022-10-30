9d34c312cccf2f79ad9426dfcee59677df8c4a7d
hs.kim <tiryul@gmail.com>
2022-10-30 08:34:53 +0000

    fix: EfficientnetV2L init

--- models/EfficientnetV2L.py
+++ models/EfficientnetV2L.py
@@ -9,7 +9,7 @@ import torchvision.models as models
 class EfficientnetV2L(nn.Module):
     def __init__(self) -> None:
         super(EfficientnetV2L, self).__init__()
-        self.backborn = models.efficientnet_v2_l()(weights=models.EfficientNet_V2_L_Weights.DEFAULT)
+        self.backborn = models.efficientnet_v2_l(weights=models.EfficientNet_V2_L_Weights.DEFAULT)
         self.classifier = nn.Sequential(
             nn.Linear(1000, 512),
             nn.BatchNorm1d(512),
