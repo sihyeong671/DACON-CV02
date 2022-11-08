c1f2449438907b41eaae96831e9ec043879e10e6
sihyeong671 <bshlab671@naver.com>
2022-11-08 13:52:43 +0000

    Fix cut mix metric

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 import datetime as dt
 from tqdm import tqdm
 
+
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 from Modules import *
@@ -55,6 +56,8 @@ def train_and_save(args: TrainArgs):
     for epoch in range(1, args.epochs+1):
         model.train()
         train_loss = []
+        train_f1 = []
+        train_acc = []
         for data in tqdm(train_loader):
             img = data['image'].float().to(args.device)
             label = data['label'].to(args.device)
@@ -69,16 +72,38 @@ def train_and_save(args: TrainArgs):
 
                 target_a = label
                 target_b = label[rand_index]
+                size_a = size
+                size_b = size[rand_index]
+                rgb_mean_a = rgb_mean
+                rgb_mean_b = rgb_mean[rand_index]
 
                 bbx1, bby1, bbx2, bby2 = rand_bbox(img.size(), lam)
                 img[:, :, bbx1:bbx2, bby1:bby2] = img[rand_index, :, bbx1:bbx2, bby1:bby2]
                 # adjust lambda to exactly match pixel ratio
                 lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (img.size()[-1] * img.size()[-2]))
                 # compute output
-                outs = model(img, size, rgb_mean)
-                loss = criterion(outs, target_a) * lam + criterion(outs, target_b) * (1. - lam)
+                size = size_a * lam + size_b * (1. - lam)
+                rgb_mean = rgb_mean_a * lam + rgb_mean_b * (1. - lam)
+                outs_a = model(img, size_a, rgb_mean_a)
+                outs_b = model(img, size_b, rgb_mean_b)
+
+                model_preds_a = outs_a.argmax(1).detach().cpu().numpy().tolist()
+                model_preds_b = outs_b.argmax(1).detach().cpu().numpy().tolist()
+
+                train_f1_a = competition_metric(target_a, model_preds_a) * lam
+                train_f1_b = competition_metric(target_b, model_preds_b) * (1. - lam)
+                train_f1.append(train_f1_a+train_f1_b)
+
+                train_acc_a = (target_a==outs_a.argmax(1)).sum().item() * lam
+                train_acc_b = (target_b==outs_b.argmax(1)).sum().item() * (1. - lam)
+                train_acc.append(train_acc_a + train_acc_b)
+
+                loss = criterion(outs_a, target_a) * lam + criterion(outs_b, target_b) * (1. - lam)
             else:
                 outs = model(img, size, rgb_mean)
+                model_preds = outs.argmax(1).detach().cpu().numpy().tolist()
+                train_f1_item = competition_metric(label, model_preds)
+                train_f1.append(train_f1_item)
                 loss = criterion(outs, label)
 
 
@@ -89,16 +114,17 @@ def train_and_save(args: TrainArgs):
             train_loss.append(loss.item())
 
         tr_loss = np.mean(train_loss)
+        train_f1_score = np.mean(train_f1)
+        train_acc_score = np.mean(train_acc)
             
-        val_loss, val_score = validation(model, criterion, val_loader, args.device)
             
-        print(f'Epoch [{epoch}], Train Loss : [{tr_loss:.5f}] Val Loss : [{val_loss:.5f}] Val F1 Score : [{val_score:.5f}]')
-        wandb.log({"train/tr_loss per epoch":tr_loss, "train/val_loss per epoch":val_loss, "train/f1 score":val_score})
+        print(f'Epoch [{epoch}], Train Loss : [{tr_loss:.5f}] Train Acc : [{train_acc_score:.5f}] Train F1 Score : [{train_f1_score:.5f}]')
+        wandb.log({"Train Loss": tr_loss, "Train Acc":train_acc_score, "Train F1 Score":train_f1_score})
         if scheduler is not None:
             scheduler.step()
-        # wandb.watch(model)
-        if best_score < val_score:
-            best_score = val_score
+            
+        if best_score < train_f1_score:
+            best_score = train_f1_score
             args.save_weight_name = f'{args.epochs}_best_{model.__class__.__name__}'
             save_model(model, args, os.path.join(args.model_weight_path, args.save_weight_name))
         
