23806c989803ad49637811ae1e4f81675ea06a0f
sihyeong671 <bshlab671@naver.com>
2022-11-08 14:57:13 +0000

    remove test transform

--- train.py
+++ train.py
@@ -32,11 +32,6 @@ def train_and_save(args: TrainArgs):
                             ToTensorV2()
                             ])
 
-    test_transform = A.Compose([
-                            A.Resize(args.img_size,args.img_size),
-                            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
-                            ToTensorV2()
-                            ])
 
     train_dataset = CustomDatasetV2(train_df_path, train_df_label, train_transform)
     train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=2)
