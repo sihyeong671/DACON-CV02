76bbc1048fe942fd184425f427ca00f294c1b0e3
sihyeong671 <bshlab671@naver.com>
2022-11-09 00:08:30 +0000

    WIP, fin size rgb

--- Modules/CustomModel/EfficientNet.py
+++ Modules/CustomModel/EfficientNet.py
@@ -47,3 +47,38 @@ class EfficientNet_B4(nn.Module):
         x = torch.cat((x, lin, rgb), 1) # 여기 cat부분이 cnn부분 끝나고 분류하는 layer들어가기 전에 붙인 부분입니다.
         x = self.clf(x)
         return x
+
+
+class EfficientNet_B4_non_rgb(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.backbone = timm.create_model('efficientnet_b4', pretrained=True)
+        self.backbone.classifier = nn.Sequential()
+        self.drop = nn.Dropout(0.4, inplace=True)
+
+        # size
+        self.size_fc = nn.Sequential(
+            nn.Linear(2, 128, bias=False),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3, inplace=True),
+            nn.Linear(128, 256)
+        )
+
+        self.clf = nn.Sequential(
+            nn.Linear(1792+256, 1024, bias=False),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.4, inplace=True),
+            nn.Linear(1024, num_classes) # + len(장르, 국가)
+        )
+        
+    def forward(self, x, size, rgb_mean):
+        x = self.backbone(x)
+        x = self.drop(x)
+
+        lin = self.size_fc(size)
+
+        x = torch.cat((x, lin), 1) # 여기 cat부분이 cnn부분 끝나고 분류하는 layer들어가기 전에 붙인 부분입니다.
+        x = self.clf(x)
+        return x
